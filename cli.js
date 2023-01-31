72e847ae68752337ce2f4851f5e20cdbf08fc159
Madison Lester <madiali@ad.unc.edu>
2023-01-31 11:38:45 -0500

    Help text and timezone

--- cli.js
+++ cli.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv.h !== undefined) {
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+        -h            Show this help message and exit.
+        -n, -s        Latitude: N positive; S negative.
+        -e, -w        Longitude: E positive; W negative.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
+        -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+        -j            Echo pretty JSON from open-meteo API and exit.`)
+        exit(0)
+}
+
+const timezone = moment.tz.guess()
