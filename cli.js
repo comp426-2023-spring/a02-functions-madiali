5744e67a97b57f233f49b58512acdf505a2d3a00
Madison Lester <madiali@ad.unc.edu>
2023-02-01 16:04:05 -0500

    First attempt

--- cli.js
+++ cli.js
@@ -19,10 +19,13 @@ if (argv.h !== undefined) {
 }
 
 // Construct url
-const latitude = (argv.n) ? parseFloat(argv.n) : (argv.s) ? -1 * parseFloat(argv.s) : undefined
-const longitude = (argv.e) ? parseFloat(argv.e) : (argv.w) ? -1 * parseFloat(argv.w) : undefined
-const timezone = (argv.z) ? argv.z : moment.tz.guess()
-const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longtiude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours"
+const latitude = argv.n ? parseFloat(argv.n) : argv.s ? -1 * parseFloat(argv.s) : undefined
+const longitude = argv.e ? parseFloat(argv.e) : argv.w ? -1 * parseFloat(argv.w) : undefined
+if (latitude == undefined || longitude == undefined) {
+    process.exit(-1)
+}
+const timezone = argv.z ? argv.z : moment.tz.guess()
+const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longitude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours"
 
 // Make a request
 const response = await fetch(url)
@@ -37,13 +40,20 @@ if (argv.j !== undefined) {
 }
 
 // Response text
+const days = (argv.d !== undefined) ? argv.d : 1
+let dayString = ""
 
-
-const days = argv.d 
 if (days == 0) {
-  console.log("today.")
-} else if (days > 1) {
-  console.log("in " + days + " days.")
-} else {
-  console.log("tomorrow.")
+    dayString = "today."
+  } else if (days > 1) {
+    dayString = "in " + days + " days."
+  } else {
+    dayString = "tomorrow."
+  }
+
+if (data.daily.precipitation_hours[days] > 0) {
+    console.log("You might need your galoshes " + dayString)
+}
+else {
+    console.log("You probably won't need your galoshes " + dayString)
 }
