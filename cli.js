c16bab37d20c48b2207db6cc0d403bc045373731
Madison Lester <madiali@ad.unc.edu>
2023-02-01 15:30:05 -0500

    Working on url for fetch

--- cli.js
+++ cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-var argv = require('minimist')(process.argv.slice(2));
+import minimist from 'minimist'
+import moment from 'moment-timezone'
+
+var argv = minimist(process.argv.slice(2));
 
 if (argv.h !== undefined) {
     console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
@@ -10,7 +13,12 @@ if (argv.h !== undefined) {
         -z            Time zone: uses tz.guess() from moment-timezone by default.
         -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
         -j            Echo pretty JSON from open-meteo API and exit.`)
-        exit(0)
+        process.exit(0)
 }
 
-const timezone = moment.tz.guess()
+const latitude = (argv.n) ? parseFloat(argv.n) : (argv.s) ? -1 * parseFloat(argv.s) : undefined
+const longitude = (argv.e) ? parseFloat(argv.e) : (argv.w) ? -1 * parseFloat(argv.w) : undefined
+const timezone = (argv.z) ? argv.z : moment.tz.guess()
+console.log(latitude + " " + longitude + " " + timezone)
+
+let url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longtiude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours"
