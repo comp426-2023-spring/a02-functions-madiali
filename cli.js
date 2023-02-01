de01199bf639955ad855ea2e17081590f560e961
Madison Lester <madiali@ad.unc.edu>
2023-02-01 15:44:40 -0500

    Progress

--- cli.js
+++ cli.js
@@ -2,9 +2,11 @@
 
 import minimist from 'minimist'
 import moment from 'moment-timezone'
+import fetch from 'node-fetch'
 
 var argv = minimist(process.argv.slice(2));
 
+// Help message
 if (argv.h !== undefined) {
     console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
         -h            Show this help message and exit.
@@ -16,9 +18,32 @@ if (argv.h !== undefined) {
         process.exit(0)
 }
 
+// Construct url
 const latitude = (argv.n) ? parseFloat(argv.n) : (argv.s) ? -1 * parseFloat(argv.s) : undefined
 const longitude = (argv.e) ? parseFloat(argv.e) : (argv.w) ? -1 * parseFloat(argv.w) : undefined
 const timezone = (argv.z) ? argv.z : moment.tz.guess()
-console.log(latitude + " " + longitude + " " + timezone)
+const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longtiude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours"
 
-let url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longtiude=" + longitude + "&timezone=" + timezone + "&daily=precipitation_hours"
+// Make a request
+const response = await fetch(url)
+
+// Get the data from the request
+const data = await response.json();
+
+// Output JSON
+if (argv.j !== undefined) {
+    console.log(data)
+    process.exit(0)
+}
+
+// Response text
+
+
+const days = argv.d 
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
